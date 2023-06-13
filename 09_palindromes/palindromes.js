2dafe2dd9d16a8f57b24e53e87a38055a58e1c58
damiangohrh123 <damiangohrh@gmail.com>
2023-06-14 00:45:28 +0800

    Completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    const regex = /[^a-zA-Z0-9]/g;
+    const processedString = string.toLowerCase().replace(regex, '');
+    return processedString.split('').reverse().join('') === processedString;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
