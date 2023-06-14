0ea1a8d00c9a5a61b78ff76308fe01e0d197db83
damiangohrh123 <damiangohrh@gmail.com>
2023-06-14 14:24:54 +0800

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(value) {
+    if (value < 0) return 'OOPS';
+    let a = 1;
+    let b = 0;
+    for (let i = 0; i < value; i ++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
